bc1a092fe65a13df1ecc7eb66cc2d14fe7798f15
fmensah1 <fmensah1@my.westga.edu>
2024-09-18 16:14:13 -0400

    Created the method stubs
    Created method stubs that when implemented, will handle calculating the subtotal, tax, tip, and total for an Array of BillItem objects.

--- code/Bill/src/edu/westga/cs1302/bill/model/BillCalculator.java
+++ code/Bill/src/edu/westga/cs1302/bill/model/BillCalculator.java
@@ -1,5 +1,26 @@
 package edu.westga.cs1302.bill.model;
 
-public class BillCalculator {
+/**
+ * Calculate the bill of a list of items
+ *  
+ *  @author felix
+ *  @version Fall 2024 
+ */
 
+public class BillCalculator {
+	
+	public static double subtotal(BillItem items[]) {
+		return 0.0;
+	}
+	
+	public static double tax(Bill items[]) {
+		return 0.0;
+	}
+	public static double tip (Bill items[]) {
+		return 0.0;
+	}
+	public static double total(Bill items[]) {
+		return 0.0;
+	}
+	
 }
