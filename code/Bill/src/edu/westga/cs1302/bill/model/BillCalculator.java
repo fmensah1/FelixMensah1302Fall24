1d14e07f47ec34b6bedebb0fd72884e357b2f04f
fmensah1 <fmensah1@my.westga.edu>
2024-09-19 06:21:56 -0400

    Implemented the methods
    Removed the methods from the billItem class an implemented them in the Billcalculator class. Changed thos methods into static so that they be accessed anywhere.

--- code/Bill/src/edu/westga/cs1302/bill/model/BillCalculator.java
+++ code/Bill/src/edu/westga/cs1302/bill/model/BillCalculator.java
@@ -2,25 +2,58 @@ package edu.westga.cs1302.bill.model;
 
 /**
  * Calculate the bill of a list of items
- *  
- *  @author felix
- *  @version Fall 2024 
+ * 
+ * @author felix
+ * @version Fall 2024
  */
 
 public class BillCalculator {
-	
-	public static double subtotal(BillItem items[]) {
-		return 0.0;
+
+	/**
+	 * Return the subtotal for the bill
+	 * 
+	 * @param items the list of items
+	 * @return the subtotal for the bill
+	 */
+	public static double getSubTotal(BillItem[] items) {
+		double subTotal = 0.0;
+		for (BillItem item : items) {
+			if (item != null) {
+				subTotal += item.getAmount();
+			}
+		}
+		return subTotal;
 	}
-	
-	public static double tax(Bill items[]) {
-		return 0.0;
+
+	/**
+	 * Return the tax for the bill
+	 * 
+	 * @param items the list of items
+	 * 
+	 * @return the tax for the bill
+	 */
+	public static double getTax(BillItem[] items) {
+		return getSubTotal(items) * 0.1;
 	}
-	public static double tip (Bill items[]) {
-		return 0.0;
+
+	/**
+	 * Return the tip for the bill
+	 * 
+	 * @param items the list of items
+	 * @return the tip for the bill
+	 */
+	public static double getTip(BillItem[] items) {
+		return getSubTotal(items) * 0.2;
 	}
-	public static double total(Bill items[]) {
-		return 0.0;
+
+	/**
+	 * Return the total for the bill
+	 * 
+	 * @param items the list of items
+	 * @return the total for the bill
+	 */
+	public static double getTotal(BillItem[] items) {
+		return getSubTotal(items) + getTax(items) + getTip(items);
 	}
-	
+
 }
