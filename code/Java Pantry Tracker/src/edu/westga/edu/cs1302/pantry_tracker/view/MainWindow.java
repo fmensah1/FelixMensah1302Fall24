e95dcab40866a6d2c61dd1fc82c837b49ccd2546
fmensah1 <fmensah1@my.westga.edu>
2024-09-25 09:58:24 -0400

    Adding Features To UI
    Added a label and textfield for food name, label and combobox for food type, a button to submit food and listview to dispalay items.

--- code/Java Pantry Tracker/src/edu/westga/edu/cs1302/pantry_tracker/view/MainWindow.java
+++ code/Java Pantry Tracker/src/edu/westga/edu/cs1302/pantry_tracker/view/MainWindow.java
@@ -6,6 +6,12 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+/**
+ * Code behind for the MainWindow of the application
+ * 
+ * @author CS 1302
+ * @version Fall 2024
+ */
 public class MainWindow {
 
     @FXML
