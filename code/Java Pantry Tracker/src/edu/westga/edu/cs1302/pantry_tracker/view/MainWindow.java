ffa358de770a9d46b10704c42379d45fa7f0c66a
fmensah1 <fmensah1@my.westga.edu>
2024-09-25 09:53:00 -0400

    Adding Features To UI
    Added a label and textfield for food name, label and combobox for food type, a button to submit food and listview to dispalay items.

--- code/Java Pantry Tracker/src/edu/westga/edu/cs1302/pantry_tracker/view/MainWindow.java
+++ code/Java Pantry Tracker/src/edu/westga/edu/cs1302/pantry_tracker/view/MainWindow.java
@@ -1,54 +1,24 @@
 package edu.westga.edu.cs1302.pantry_tracker.view;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-/**
- * Code behind for the MainWindow of the application.
- * 
- * 
- *  @author CS 1302
- *  @veersion Fall 2024
- */
 public class MainWindow {
 
     @FXML
-    private ResourceBundle resources;
+    private ComboBox<?> foodTypeComboBox;
 
     @FXML
-    private URL location;
+    private ListView<?> foods;
 
     @FXML
     private TextField name;
 
     @FXML
-    private ListView<String> student;
-    
-    @FXML
-    private ComboBox<String> foodType;
-
-
-    @FXML
-    void addStudent(ActionEvent event) {
-    	String studentName = this.name.getText();
-    	this.student.getItems().add(studentName);
-
-    }
-
-    @FXML
-    void removeStudent(ActionEvent event) {
-
-    }
-
-    @FXML
-    void initialize() {
-        assert this.name != null : "fx:id=\"name\" was not injected: check your FXML file 'MainWindow.fxml'.";
-        assert this.student != null : "fx:id=\"student\" was not injected: check your FXML file 'MainWindow.fxml'.";
+    void addFood(ActionEvent event) {
 
     }
 
